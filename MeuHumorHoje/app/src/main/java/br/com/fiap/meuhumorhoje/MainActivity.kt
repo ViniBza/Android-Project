d22962d531723ee600cf0eb6826823761389feeb
ViniBza <ViniBza@gmail.com>
2025-09-17 12:33:02 +0100

    Projeto Humor finalizado

--- MeuHumorHoje/app/src/main/java/br/com/fiap/meuhumorhoje/MainActivity.kt
+++ MeuHumorHoje/app/src/main/java/br/com/fiap/meuhumorhoje/MainActivity.kt
@@ -1,25 +1,23 @@
 package br.com.fiap.meuhumorhoje
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsTopHeight
-import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -27,18 +25,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.fiap.meuhumorhoje.ui.theme.MeuHumorHojeTheme
-import org.intellij.lang.annotations.JdkConstants
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,14 +62,17 @@ class MainActivity : ComponentActivity() {
 val TopPadding = 20.dp
 @Composable
 fun Greeting( modifier: Modifier = Modifier) {
+    val numeroHumor =  remember { mutableIntStateOf(0) }
+
     var visibily by remember {
         mutableStateOf(false)
+
     }
     Box(
     )  {
         Column(modifier = modifier.fillMaxSize()){
 
-        Spacer( modifier = Modifier.height(TopPadding))
+        Spacer( modifier = Modifier.height(TopPadding) )
 
         Text(
             text = "Meu Humor Hoje",
@@ -75,7 +81,7 @@ fun Greeting( modifier: Modifier = Modifier) {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
 
-         Spacer( modifier = Modifier.height(TopPadding))
+         Spacer( modifier = Modifier.height(TopPadding) )
 
         Row (modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround ) {
@@ -93,16 +99,15 @@ fun Greeting( modifier: Modifier = Modifier) {
         Row ( modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceAround) {
 
-            OutlinedButton(onClick = { /*TODO*/ }) {
+            OutlinedButton(onClick = { numeroHumor.value-- }) {
                 Text( text = "-")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text( text = "+")
+            OutlinedButton(onClick = { numeroHumor.value++ }) {
+                Text( text = "+" )
             }
         }
 
        Column (modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-
            Text(
                text = "Selecione um número de 0 a 10.",
                fontSize = 18.sp,
@@ -112,31 +117,51 @@ fun Greeting( modifier: Modifier = Modifier) {
            Spacer(modifier = Modifier.height(TopPadding))
 
            Text(
-               text = "Seu número é: ",
-               fontSize = 16.sp,
-               style = MaterialTheme.typography.displaySmall,
-               )
+               buildAnnotatedString {
+                   append("Sua idade é: ")
+                   withStyle(style = SpanStyle(color = Color.Red)) { append("${numeroHumor.value}") }
+               }
+           )
 
            Spacer(modifier = Modifier.height(TopPadding))
 
-           ElevatedButton(onClick ={visibily = !visibily} ) {
+           ElevatedButton(onClick = {visibily = !visibily} ) {
                Text( text = "Verificar seu humor")
            }
            
            Spacer(modifier = Modifier.height(TopPadding))
 
            AnimatedVisibility(visible = visibily) {
-                Text(
-                    text = "OLA MUNDO!!!!!!!",
-                    fontSize = 16.sp,
-                )
+
+               when (numeroHumor.value) {
+
+                   in 0..4 ->
+                       Image(
+                           painterResource(R.drawable.emoticon_dead),
+                           contentDescription = "Triste",
+                           modifier = Modifier.align(Alignment.CenterHorizontally)
+                       )
+                   in 5..8 ->
+                       Image(
+                           painterResource(R.drawable.emoticon_neutral),
+                           contentDescription = "Feliz",
+                           modifier = Modifier.align(Alignment.CenterHorizontally)
+                       )
+
+                   else ->
+                       Image(
+                           painterResource(R.drawable.emoticon_cool),
+                           contentDescription = "Normal",
+                           modifier = Modifier.align(Alignment.CenterHorizontally)
+                       )
+
+               }
                }
 
        }
      }
    }
 }
-
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
